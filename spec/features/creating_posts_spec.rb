61640c277297c3ef2b9bb889f6a0e2b8b9fd5fb3
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-03 17:07:11 -0400

    updates edit spec for updating post

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Posts" do
-  
-  DatabaseCleaner.strategy = :transaction
-  DatabaseCleaner.start
-  
-  DatabaseCleaner.clean
 
   before do 
     @joe = User.create!(email: "joe@example.com", password: "password")
